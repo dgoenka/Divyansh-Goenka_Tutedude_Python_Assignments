0deda1f1c685eb012f50fc7c5657a38f4a91de85
Divyansh Goenka <divyansh.goenka@gmail.com>
2025-03-18 13:24:31 +0530

    Initial Commit

---
+++ task_one.py
@@ -0,0 +1,6 @@
+a = float(input("Enter a number: "))
+b = float(input("Enter another number: "))
+print("Addition:\t\t" + str(a) + " + " + str(b) + " = " + str(a + b))
+print("Subtraction:\t\t" + str(a) + " - " + str(b) + " = " + str(a - b))
+print("Multiplication:\t\t" + str(a) + " * " + str(b) + " = " + str(a * b))
+print("Division:\t\t" + str(a) + " / " + str(b) + " = " + str(a / b))
